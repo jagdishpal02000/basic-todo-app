68f5e1797e5aff0c0336964375132648ff54306b
Jagdish Pal <59500712+jagdishpal02000@users.noreply.github.com>
2022-03-19 10:08:16 +0530

    task.js file deleted

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import { useEffect, useRef, useState } from "react";
-import Task from "./task";
+// import Task from "./task";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
-// const tasks = ["hii", "jagdish", "happy holi"];
 const App = () => {
   const [tasks, setTasks] = useState([]);
   const [userInput, setUserInput] = useState("");
   const enteredTask = useRef(null);
 
+  const taskTitle = useRef(null);
+
   const taskAdder = () => {
     if (userInput.length) {
+      const task_obj = { id: new Date().getTime().toString(), name: userInput };
       setTasks((prev_tasks) => {
-        return [...prev_tasks, userInput];
+        console.log(task_obj);
+        return [...prev_tasks, task_obj];
       });
       setUserInput("");
     } else {
       enteredTask.current.select();
     }
+  };
+
+  const editTask = (taskId) => {
+    taskTitle.current.disabled = false;
+  };
+
+  const deleteTask = (taskId) => {
+    const newTasks = tasks.filter((task) => {
+      return task.id !== taskId;
+    });
+    setTasks(newTasks);
   };
 
   useEffect(() => {
@@ -38,7 +54,30 @@ const App = () => {
       </div>
       <section className="task-list">
         {tasks.map((task) => {
-          return <Task task={task} />;
+          return (
+            <div key={task.id} className="task">
+              <textarea
+                ref={taskTitle}
+                maxLength="200"
+                className="title"
+                disabled
+                value={task.name}
+                onChange={(e) => {
+                  console.log(e.target.value);
+                }}
+              />
+              <FontAwesomeIcon
+                className="edit"
+                onClick={() => editTask(task.id)}
+                icon={faPenToSquare}
+              />
+              <FontAwesomeIcon
+                onClick={() => deleteTask(task.id)}
+                className="trash"
+                icon={faTrashAlt}
+              />
+            </div>
+          );
         })}
       </section>
     </>
